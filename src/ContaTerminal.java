4cf111c40b6b350544a77c017ff1da3cd2d09d59
ricardoDevTechBsb <desenvolvedorbsb@gmail.com>
2025-06-19 15:52:58 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -13,22 +13,21 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.printf("Por favor, digite o número da Agência !");
-        scanner.nextLine();
         numero  = scanner.nextInt();
+        scanner.nextLine();
 
         System.out.printf("Por favor, digite a Agência !");
-        scanner.nextLine();
         agencia = scanner.next();
+        scanner.nextLine();
 
         System.out.printf("Por favor, digite o nome do Cliente !");
-        scanner.nextLine();
         nomeCliente = scanner.next();
+        scanner.nextLine();
 
         System.out.printf("Por favor, digite o saldo !");
-        scanner.nextLine();
         saldo = scanner.nextDouble();
-
         scanner.nextLine();
+
         System.out.printf("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
     }
